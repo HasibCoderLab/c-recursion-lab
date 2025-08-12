844fb8c974bb8f9f9cdba6acbb2ea069490f87fd
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-12 08:06:52 +0600

    solve 1st problem

--- solve_problem.c
+++ solve_problem.c
@@ -1,5 +1,26 @@
 //  ============================================================
 //                    📌 Practice Problems
 //                    Topic : C Programming - Recursion
-//                   
+//
 // ============================================================
+
+// 👉👉  🔹Qu -1 🔹 print "Hello Wrold " 5 times
+
+#include <stdio.h>
+void print_HW(int number);
+int main()
+{
+    print_HW(5);
+    return 0;
+}
+
+void print_HW(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+
+    printf("Hello World \n");
+    print_HW(number - 1);
+}
